f6703c5fc374e93ac06275b3274ec78b18dbbd58
joinyi <hu141418@gmail.com>
2019-06-18 16:28:11 +0800

    fixed version

--- src/lib/storage.js
+++ src/lib/storage.js
@@ -54,7 +54,7 @@ class Storage extends ScratchStorage {
         this.assetHost = assetHost;
     }
     getAssetGetConfig (asset) {
-        return `${this.assetHost}/internalapi/asset/${asset.assetId}.${asset.dataFormat}`;
+        return `${this.assetHost}/scratch/asset/${asset.assetId}.${asset.dataFormat}`;
     }
     getAssetCreateConfig (asset) {
         return {
