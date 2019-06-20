1662b5fd84139b4710b2973ebf00d52e3bf72361
joinyi <hu141418@gmail.com>
2019-06-20 16:37:58 +0800

    fixed version gui

--- src/lib/storage.js
+++ src/lib/storage.js
@@ -63,7 +63,7 @@ class Storage extends ScratchStorage {
             // assetId as part of the create URI. So, force the method to POST.
             // Then when storage finds this config to use for the "update", still POSTs
             method: 'post',
-            url: `${this.assetHost}/${asset.assetId}.${asset.dataFormat}`,
+            url: `${this.projectHost}/${asset.assetId}.${asset.dataFormat}`,
             withCredentials: true
         };
     }
